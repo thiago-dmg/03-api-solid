2894ceb0311a7d44024c8d9e2bc28bab41ab3962
Thiago <bussoladev@gmail.com>
2024-02-13 20:17:45 -0300

    feat(*) Test Controllers

---
+++ src/http/controllers/authenticate.ts
@@ -0,0 +1,34 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { AuthenticateUseCase } from "@/use-cases/authenticate";
+import { InvalidCredenctialsError } from "@/use-cases/errors/invalid-credentials-error";
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodyShema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { email, password } = authenticateBodyShema.parse(request.body);
+
+  try {
+    const usersRepository = new PrismaUsersRepository();
+    const authenticateUseCase = new AuthenticateUseCase(usersRepository);
+    await authenticateUseCase.execute({
+      email,
+      password,
+    });
+  } catch (err) {
+    if (err instanceof InvalidCredenctialsError) {
+      return reply.status(400).send({ message: err.message });
+    }
+
+    throw err;
+  }
+
+  return reply.status(200).send();
+}
