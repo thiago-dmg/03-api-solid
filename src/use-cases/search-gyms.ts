e11423dc204fcafea75d0925129180d600bbf8d9
Thiago <bussoladev@gmail.com>
2024-02-16 17:07:11 -0300

    feat(*): Use Case Search Gym for name

---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,24 @@
+import { Gym } from "@prisma/client";
+import { GymsRepository } from "@/repositories/gyms-repository";
+
+interface SearchGymsUseCaseRequest {
+  query: string;
+  page: number;
+}
+
+interface SearchGymsUseCaseReponse {
+  gyms: Gym[];
+}
+
+export class SearchGymsUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsUseCaseRequest): Promise<SearchGymsUseCaseReponse> {
+    const gyms = await this.gymsRepository.searchMany(query, page);
+
+    return { gyms };
+  }
+}
