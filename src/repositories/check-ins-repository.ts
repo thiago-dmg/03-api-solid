502ed6e4c7c0c704f33f594d351e9e2042181a9e
Thiago <bussoladev@gmail.com>
2024-02-15 21:11:32 -0300

    feat(*): Create Use Case History and Pagination

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,6 +1,7 @@
 import { Checkin, Prisma } from "@prisma/client";
 
 export interface CheckInsRepository {
-  create(data: Prisma.CheckinUncheckedCreateInput): Promise<Checkin>;
   findByUserIdOnDate(userId: string, date: Date): Promise<Checkin | null>;
+  findManyByUserId(userId: string, page: number): Promise<Checkin[]>;
+  create(data: Prisma.CheckinUncheckedCreateInput): Promise<Checkin>;
 }
