fa57d0450b6780c47b6b8fadefc2142ae3b9dffd
Thiago <bussoladev@gmail.com>
2024-02-19 18:41:52 -0300

    feat(*): Create test environment

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,13 @@
+import { Environment } from "vitest";
+
+export default <Environment>{
+  name: "prisma",
+  transformMode: "web",
+  async setup() {
+    console.log("Executou");
+
+    return {
+      async teardown() {},
+    };
+  },
+};
