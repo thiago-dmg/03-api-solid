d4817f150e66b67ade86f169e3e704137289cb2c
Thiago <bussoladev@gmail.com>
2024-02-21 14:37:27 -0300

    feat(*): Refresh Token & RBAC

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -1,10 +1,9 @@
-// fastify-jwt.d.ts
 import "@fastify/jwt";
-
 declare module "@fastify/jwt" {
   export interface FastifyJWT {
     user: {
+      role: "ADMIN" | "MEMBER";
       sub: string;
-    }; // user type is return type of `request.user` object
+    };
   }
 }
